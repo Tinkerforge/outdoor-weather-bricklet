fd64c4714d67000d32f5fbf660a0b42f5a2980b4
Olaf Lüke <olaf@tinkerforge.com>
2017-08-30 14:40:51 +0200

    Add API implementation

--- software/src/communication.c
+++ software/src/communication.c
@@ -22,20 +22,134 @@
 #include "communication.h"
 
 #include "bricklib2/utility/communication_callback.h"
+#include "bricklib2/hal/system_timer/system_timer.h"
 #include "bricklib2/protocols/tfp/tfp.h"
+
+#include "rfm210.h"
+
+extern RFM210 rfm210;
 
 BootloaderHandleMessageResponse handle_message(const void *message, void *response) {
 	switch(tfp_get_fid_from_message(message)) {
-
+		case FID_GET_WEATHER_STATION_IDENTIFIERS_LOW_LEVEL: return get_weather_station_identifiers_low_level(message, response);
+		case FID_GET_WEATHER_STATION_DATA: return get_weather_station_data(message, response);
+		case FID_SET_WEATHER_STATION_CALLBACK_CONFIGURATION: return set_weather_station_callback_configuration(message);
+		case FID_GET_WEATHER_STATION_CALLBACK_CONFIGURATION: return get_weather_station_callback_configuration(message, response);
 		default: return HANDLE_MESSAGE_RESPONSE_NOT_SUPPORTED;
 	}
 }
 
 
+BootloaderHandleMessageResponse get_weather_station_identifiers_low_level(const GetWeatherStationIdentifiersLowLevel *data, GetWeatherStationIdentifiersLowLevel_Response *response) {
+	response->header.length = sizeof(GetWeatherStationIdentifiersLowLevel_Response);
+
+	uint8_t response_index = 0;
+	for(; rfm210.current_id_index < RFM210_MAX_DEVICES; rfm210.current_id_index++) {
+		if(rfm210.payload_last_change[rfm210.current_id_index] != 0) {
+			response->identifiers_chunk_data[response_index] = rfm210.current_id_index;
+			response_index++;
+			if(response_index == 60) {
+				break;
+			}
+		}
+	}
+
+	response->identifiers_length = response_index;
+	response->identifiers_chunk_offset = rfm210.current_chunk_offset;
+
+	if(rfm210.current_id_index == RFM210_MAX_DEVICES) {
+		rfm210.current_id_index = 0;
+		rfm210.current_chunk_offset = 0;
+	} else {
+		rfm210.current_chunk_offset += response_index;
+	}
+
+	return HANDLE_MESSAGE_RESPONSE_NEW_MESSAGE;
+}
+
+BootloaderHandleMessageResponse get_weather_station_data(const GetWeatherStationData *data, GetWeatherStationData_Response *response) {
+	if(rfm210.payload_last_change[data->identifier] == 0) {
+		return HANDLE_MESSAGE_RESPONSE_INVALID_PARAMETER;
+	}
+
+	response->header.length = sizeof(GetWeatherStationData_Response);
+
+	RFM210Packet packet;
+	rfm210_fill_packet(&rfm210, data->identifier, &packet);
+	response->temperature    = packet.temperature;
+	response->humidity       = packet.humidity;
+	response->wind_speed     = packet.wind_speed;
+	response->gust_speed     = packet.gust_speed;
+	response->rain           = packet.rain;
+	response->wind_direction = packet.wind_direction;
+	response->battery_low    = packet.battery_low;
+	response->last_change    = ((uint32_t)(system_timer_get_ms() - rfm210.payload_last_change[data->identifier])) / 1000;
+
+	return HANDLE_MESSAGE_RESPONSE_NEW_MESSAGE;
+}
+
+BootloaderHandleMessageResponse set_weather_station_callback_configuration(const SetWeatherStationCallbackConfiguration *data) {
+	rfm210.callback_enabled = data->enable_callback;
+
+	return HANDLE_MESSAGE_RESPONSE_EMPTY;
+}
+
+BootloaderHandleMessageResponse get_weather_station_callback_configuration(const GetWeatherStationCallbackConfiguration *data, GetWeatherStationCallbackConfiguration_Response *response) {
+	response->header.length   = sizeof(GetWeatherStationCallbackConfiguration_Response);
+	response->enable_callback = rfm210.callback_enabled;
+
+	return HANDLE_MESSAGE_RESPONSE_NEW_MESSAGE;
+}
 
 
 
 
+bool handle_weather_station_data_callback(void) {
+	static bool is_buffered = false;
+	static WeatherStationData_Callback cb;
+	static uint32_t last_change[RFM210_MAX_DEVICES] = {0};
+
+	if(!rfm210.callback_enabled) {
+		return false;
+	}
+
+	if(!is_buffered) {
+		tfp_make_default_header(&cb.header, bootloader_get_uid(), sizeof(WeatherStationData_Callback), FID_CALLBACK_WEATHER_STATION_DATA);
+		uint16_t id;
+		for(id = 0; id < RFM210_MAX_DEVICES; id++) {
+			if(rfm210.payload_last_change[id] != 0 && (((uint32_t)(rfm210.payload_last_change[id] - last_change[id])) > 4)) {
+				last_change[id] = rfm210.payload_last_change[id];
+
+				RFM210Packet packet;
+				rfm210_fill_packet(&rfm210, id, &packet);
+				cb.identifier     = id;
+				cb.temperature    = packet.temperature;
+				cb.humidity       = packet.humidity;
+				cb.wind_speed     = packet.wind_speed;
+				cb.gust_speed     = packet.gust_speed;
+				cb.rain           = packet.rain;
+				cb.wind_direction = packet.wind_direction;
+				cb.battery_low    = packet.battery_low;
+
+				break;
+			}
+		}
+
+		if(id == RFM210_MAX_DEVICES) {
+			return false;
+		}
+	}
+
+	if(bootloader_spitfp_is_send_possible(&bootloader_status.st)) {
+		bootloader_spitfp_send_ack_and_message(&bootloader_status, (uint8_t*)&cb, sizeof(WeatherStationData_Callback));
+		is_buffered = false;
+		return true;
+	} else {
+		is_buffered = true;
+	}
+
+	return false;
+}
 
 void communication_tick(void) {
 	communication_callback_tick();
--- software/src/communication.h
+++ software/src/communication.h
@@ -34,6 +34,24 @@ void communication_tick(void);
 void communication_init(void);
 
 // Constants
+#define OUTDOOR_WEATHER_WIND_DIRECTION_N 0
+#define OUTDOOR_WEATHER_WIND_DIRECTION_NNE 1
+#define OUTDOOR_WEATHER_WIND_DIRECTION_NE 2
+#define OUTDOOR_WEATHER_WIND_DIRECTION_ENE 3
+#define OUTDOOR_WEATHER_WIND_DIRECTION_E 4
+#define OUTDOOR_WEATHER_WIND_DIRECTION_ESE 5
+#define OUTDOOR_WEATHER_WIND_DIRECTION_SE 6
+#define OUTDOOR_WEATHER_WIND_DIRECTION_SSE 7
+#define OUTDOOR_WEATHER_WIND_DIRECTION_S 8
+#define OUTDOOR_WEATHER_WIND_DIRECTION_SSW 9
+#define OUTDOOR_WEATHER_WIND_DIRECTION_SW 10
+#define OUTDOOR_WEATHER_WIND_DIRECTION_WSW 11
+#define OUTDOOR_WEATHER_WIND_DIRECTION_W 12
+#define OUTDOOR_WEATHER_WIND_DIRECTION_WNW 13
+#define OUTDOOR_WEATHER_WIND_DIRECTION_NW 14
+#define OUTDOOR_WEATHER_WIND_DIRECTION_NNW 15
+#define OUTDOOR_WEATHER_WIND_DIRECTION_ERROR 255
+
 #define OUTDOOR_WEATHER_BOOTLOADER_MODE_BOOTLOADER 0
 #define OUTDOOR_WEATHER_BOOTLOADER_MODE_FIRMWARE 1
 #define OUTDOOR_WEATHER_BOOTLOADER_MODE_BOOTLOADER_WAIT_FOR_REBOOT 2
@@ -53,19 +71,81 @@ void communication_init(void);
 #define OUTDOOR_WEATHER_STATUS_LED_CONFIG_SHOW_STATUS 3
 
 // Function and callback IDs and structs
+#define FID_GET_WEATHER_STATION_IDENTIFIERS_LOW_LEVEL 1
+#define FID_GET_WEATHER_STATION_DATA 2
+#define FID_SET_WEATHER_STATION_CALLBACK_CONFIGURATION 3
+#define FID_GET_WEATHER_STATION_CALLBACK_CONFIGURATION 4
 
+#define FID_CALLBACK_WEATHER_STATION_DATA 5
 
+typedef struct {
+	TFPMessageHeader header;
+} __attribute__((__packed__)) GetWeatherStationIdentifiersLowLevel;
+
+typedef struct {
+	TFPMessageHeader header;
+	uint16_t identifiers_length;
+	uint16_t identifiers_chunk_offset;
+	uint8_t identifiers_chunk_data[60];
+} __attribute__((__packed__)) GetWeatherStationIdentifiersLowLevel_Response;
+
+typedef struct {
+	TFPMessageHeader header;
+	uint8_t identifier;
+} __attribute__((__packed__)) GetWeatherStationData;
+
+typedef struct {
+	TFPMessageHeader header;
+	int16_t temperature;
+	uint8_t humidity;
+	uint16_t wind_speed;
+	uint16_t gust_speed;
+	uint32_t rain;
+	uint8_t wind_direction;
+	bool battery_low;
+	uint16_t last_change;
+} __attribute__((__packed__)) GetWeatherStationData_Response;
+
+typedef struct {
+	TFPMessageHeader header;
+	bool enable_callback;
+} __attribute__((__packed__)) SetWeatherStationCallbackConfiguration;
+
+typedef struct {
+	TFPMessageHeader header;
+} __attribute__((__packed__)) GetWeatherStationCallbackConfiguration;
+
+typedef struct {
+	TFPMessageHeader header;
+	bool enable_callback;
+} __attribute__((__packed__)) GetWeatherStationCallbackConfiguration_Response;
+
+typedef struct {
+	TFPMessageHeader header;
+	uint8_t identifier;
+	int16_t temperature;
+	uint8_t humidity;
+	uint16_t wind_speed;
+	uint16_t gust_speed;
+	uint32_t rain;
+	uint8_t wind_direction;
+	bool battery_low;
+} __attribute__((__packed__)) WeatherStationData_Callback;
 
 
 // Function prototypes
-
+BootloaderHandleMessageResponse get_weather_station_identifiers_low_level(const GetWeatherStationIdentifiersLowLevel *data, GetWeatherStationIdentifiersLowLevel_Response *response);
+BootloaderHandleMessageResponse get_weather_station_data(const GetWeatherStationData *data, GetWeatherStationData_Response *response);
+BootloaderHandleMessageResponse set_weather_station_callback_configuration(const SetWeatherStationCallbackConfiguration *data);
+BootloaderHandleMessageResponse get_weather_station_callback_configuration(const GetWeatherStationCallbackConfiguration *data, GetWeatherStationCallbackConfiguration_Response *response);
 
 // Callbacks
-
+bool handle_weather_station_data_callback(void);
 
 #define COMMUNICATION_CALLBACK_TICK_WAIT_MS 1
-#define COMMUNICATION_CALLBACK_HANDLER_NUM 0
+#define COMMUNICATION_CALLBACK_HANDLER_NUM 1
 #define COMMUNICATION_CALLBACK_LIST_INIT \
+	handle_weather_station_data_callback, \
 
 
 #endif
