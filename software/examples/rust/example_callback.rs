234ef7d523f9fefe2d78008c773f740c4282add0
Erik Fleckstein <erik@tinkerforge.com>
2018-11-07 16:25:34 +0100

    rust: Accept host names for ip connection, fix naming inconsistencies

--- software/examples/rust/example_callback.rs
+++ software/examples/rust/example_callback.rs
@@ -1,50 +1,52 @@
 use std::{error::Error, io, thread};
-use tinkerforge::{ipconnection::IpConnection, outdoor_weather_bricklet::*};
+use tinkerforge::{ip_connection::IpConnection, outdoor_weather_bricklet::*};
 
-const HOST: &str = "127.0.0.1";
+const HOST: &str = "localhost";
 const PORT: u16 = 4223;
-const UID: &str = "XYZ"; // Change XYZ to the UID of your Outdoor Weather Bricklet
+const UID: &str = "XYZ"; // Change XYZ to the UID of your Outdoor Weather Bricklet.
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let ipcon = IpConnection::new(); // Create IP connection
-    let outdoor_weather_bricklet = OutdoorWeatherBricklet::new(UID, &ipcon); // Create device object
+    let ipcon = IpConnection::new(); // Create IP connection.
+    let ow = OutdoorWeatherBricklet::new(UID, &ipcon); // Create device object.
 
-    ipcon.connect(HOST, PORT).recv()??; // Connect to brickd
-                                        // Don't use device before ipcon is connected
+    ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
+                                          // Don't use device before ipcon is connected.
 
     // Enable station data callbacks
-    outdoor_weather_bricklet.set_station_callback_configuration(true);
+    ow.set_station_callback_configuration(true);
 
     // Enable sensor data callbacks
-    outdoor_weather_bricklet.set_sensor_callback_configuration(true);
+    ow.set_sensor_callback_configuration(true);
 
-    //Create listener for station data events.
-    let station_data_listener = outdoor_weather_bricklet.get_station_data_receiver();
-    // Spawn thread to handle received events. This thread ends when the outdoor_weather_bricklet
+    // Create receiver for station data events.
+    let station_data_receiver = ow.get_station_data_receiver();
+
+    // Spawn thread to handle received events. This thread ends when the `ow` object
     // is dropped, so there is no need for manual cleanup.
     thread::spawn(move || {
-        for event in station_data_listener {
-            println!("Identifier (Station): {}", event.identifier);
-            println!("Temperature (Station): {}{}", event.temperature as f32 / 10.0, " °C");
-            println!("Humidity (Station): {}{}", event.humidity, " %RH");
-            println!("Wind Speed (Station): {}{}", event.wind_speed as f32 / 10.0, " m/s");
-            println!("Gust Speed (Station): {}{}", event.gust_speed as f32 / 10.0, " m/s");
-            println!("Rain (Station): {}{}", event.rain as f32 / 10.0, " mm");
-            println!("Wind Direction (Station): {}", event.wind_direction);
-            println!("Battery Low (Station): {}", event.battery_low);
+        for station_data in station_data_receiver {
+            println!("Identifier (Station): {}", station_data.identifier);
+            println!("Temperature (Station): {} °C", station_data.temperature as f32 / 10.0);
+            println!("Humidity (Station): {} %RH", station_data.humidity);
+            println!("Wind Speed (Station): {} m/s", station_data.wind_speed as f32 / 10.0);
+            println!("Gust Speed (Station): {} m/s", station_data.gust_speed as f32 / 10.0);
+            println!("Rain (Station): {} mm", station_data.rain as f32 / 10.0);
+            println!("Wind Direction (Station): {}", station_data.wind_direction);
+            println!("Battery Low (Station): {}", station_data.battery_low);
             println!();
         }
     });
 
-    //Create listener for sensor data events.
-    let sensor_data_listener = outdoor_weather_bricklet.get_sensor_data_receiver();
-    // Spawn thread to handle received events. This thread ends when the outdoor_weather_bricklet
+    // Create receiver for sensor data events.
+    let sensor_data_receiver = ow.get_sensor_data_receiver();
+
+    // Spawn thread to handle received events. This thread ends when the `ow` object
     // is dropped, so there is no need for manual cleanup.
     thread::spawn(move || {
-        for event in sensor_data_listener {
-            println!("Identifier (Sensor): {}", event.identifier);
-            println!("Temperature (Sensor): {}{}", event.temperature as f32 / 10.0, " °C");
-            println!("Humidity (Sensor): {}{}", event.humidity, " %RH");
+        for sensor_data in sensor_data_receiver {
+            println!("Identifier (Sensor): {}", sensor_data.identifier);
+            println!("Temperature (Sensor): {} °C", sensor_data.temperature as f32 / 10.0);
+            println!("Humidity (Sensor): {} %RH", sensor_data.humidity);
             println!();
         }
     });
